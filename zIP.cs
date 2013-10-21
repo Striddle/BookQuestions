03e63330bb761585aeb239feadf490a9f667213b
Striddle <Striddle@hotmail.com>
2013-10-21 08:40:59 -0400

    zips

---
+++ zIP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ZipCodes = new int[10];
+            ZipCodes[1] = 00001;
+            ZipCodes[2] = 00002;
+            ZipCodes[3] = 00003;
+            ZipCodes[4] = 00004;
+            ZipCodes[5] = 00005;
+            ZipCodes[6] = 00006;
+            ZipCodes[7] = 00007;
+            ZipCodes[8] = 00008;
+            ZipCodes[9] = 00009;
+            ZipCodes[10] = 00010;
+
+            Console.WriteLine("WHAT is your zipcode?");
+            string input = Console.ReadLine();
+
+        }
+    }
+}
